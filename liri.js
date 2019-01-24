d4cbce6c14d5b4fa99932f3b450a86c47bc41bae
dsb67 <diannasb67@gmail.com>
2019-01-24 13:07:02 -0800

    fixed do what it says and movie

--- liri.js
+++ liri.js
@@ -6,33 +6,31 @@ var keys = require("./keys.js");
 
 var Spotify = require('node-spotify-api');
 
-var spotify = new Spotify(keys.spotify);
+var request = require("request");
 
 var programToRun = process.argv[2];
 
 var action = process.argv[3]; //spotify
-
-var getMovie = process.argv[4]; //movie
-
-var request = require("request");
 
 
 if (programToRun === "spotify-this-song") {
     spotifyThisSong(action);
 
 } else if (programToRun === "movie-this") {
-    movieThis(getMovie);
+    movieThis(action);
 
 } else if (programToRun === "do-what-it-says") {
-    doWhatItSays();
+    doWhatItSays(action);
 
 } else {
     console.log("error check entry")
 
 }
 
+//CODE FOR SPOTIFY//
 
 function spotifyThisSong(song = '') {
+    var spotify = new Spotify(keys.spotify);
     spotify.search({ type: 'track', query: song }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
@@ -51,25 +49,40 @@ function spotifyThisSong(song = '') {
 
 }
 
-function movieThis(title = "") {
-    // getMovie = "";
+//CODE FOR OMBDAPI
+function movieThis(movieName) {
 
-    // request('http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy', function (error, response, body) {
-    //     console.log("Title: " + JSON.parse(body).Title);
-    //     console.log("Release Year: " + JSON.parse(body).Year);
-    //     console.log("IMDB Ratings: " + JSON.parse(body).imdbRating);
-    //     console.log("Rotten Tomatoes Rating: " + JSON.parse(body).RottenTomatoes);
-    //     console.log("Country: " + JSON.parse(body).Country);
-    //     console.log("Language: " + JSON.parse(body).Language);
-    //     console.log("Plot: " + JSON.parse(body).Plot);
-    //     console.log("Actors: " + JSON.parse(body).Actors);
-    // })
+    request('http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey=trilogy', function (error, response, body) {
+        console.log("Title: " + JSON.parse(body).Title);
+        console.log("Release Year: " + JSON.parse(body).Year);
+        console.log("IMDB Ratings: " + JSON.parse(body).imdbRating);
+        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).RottenTomatoes);
+        console.log("Country: " + JSON.parse(body).Country);
+        console.log("Language: " + JSON.parse(body).Language);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
+    })
 }
 
+
+//CODE FOR DO WHAT IT SAYS
 
 function doWhatItSays() {
-    console.log("running do what it says")
-}
+
+    fs.readFile("./random.txt", "utf8", function (err, data) {
+        if (err) return console.log(err);
+        var thingsArr = data.split(",");
+        thingsArr.forEach(function (thing) {
+            console.log(thing.trim());
+        })
+
+        console.log("= = = = = = = = = = = = = = ");
+        for (var i = 0; i < thingsArr.length; i++) {
+            console.log(thingsArr[i].trim());
+        }
+
+    });
 
 
 
+};
